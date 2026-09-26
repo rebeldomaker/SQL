32e7b88f7cf0f29c8bebca9603278f3addcb587d
Reb <rebeldomaker@icloud.com>
2026-09-26 13:25:28 +0200

    Update DB_Boghandel.sql

--- Intro Assigment/Bibliotek/DB_Boghandel.sql
+++ Intro Assigment/Bibliotek/DB_Boghandel.sql
@@ -3,5 +3,5 @@ CREATE database DB_Boghandel;
 CREATE OR REPLACE TABLE Books
 ( -- auto-incrementing kronologiskai prideda nauja skaiciu, naujam item'sui pvz. knygai
     id   INT AUTO_INCREMENT PRIMARY KEY, -- a primary key is similar kind of to IDs, but PKs do not need to be numbers, just a way to assign a unique identifier
-    name
+    Title
 )
