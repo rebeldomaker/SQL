6bcecf7c3c3f8cac9546cd1422a392285feb28d1
Reb <rebeldomaker@icloud.com>
2026-09-26 21:15:07 +0200

    phone number needs to be number var, not int

--- Intro Assigment/Bibliotek/DB_Boghandel.sql
+++ Intro Assigment/Bibliotek/DB_Boghandel.sql
@@ -1,64 +1,37 @@
-CREATE or REPLACE database DB_Boghandel;
+CREATE DATABASE IF NOT EXISTS DB_Boghandel;
 USE DB_Boghandel;
--- "or replace" makes this basically re-usable code
+
 CREATE OR REPLACE TABLE Books
-(                                          -- auto-incrementing kronologiskai prideda nauja skaiciu, naujam item'sui pvz. knygai
-    id     INT AUTO_INCREMENT PRIMARY KEY, -- a primary key is similar kind of to IDs, but PKs do not need to be numbers, just a way to assign a unique identifier
-    title  text(512),                      -- char by default is 256
-    author text(512),
-    genre  text(128),
-    price  dec
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    title  VARCHAR(512),
+    author VARCHAR(512),
+    genre  VARCHAR(128),
+    price  DECIMAL(10, 2)
 );
 
 CREATE OR REPLACE TABLE Customers
-(                                                    -- auto-incrementing kronologiskai prideda nauja skaiciu, naujam item'sui pvz. knygai
-    id               INT AUTO_INCREMENT PRIMARY KEY, -- a primary key is similar kind of to IDs, but PKs do not need to be numbers, just a way to assign a unique identifier
-    name             char,                           -- char by default is 256
-    address          char,
-    telephone_number int,
-    purchased_books  int
+(
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    name             VARCHAR(512),
+    address          VARCHAR(512),
+    telephone_number VARCHAR(128),
+    purchased_books  INT
 );
 
 INSERT INTO Books
     (title, author, genre, price)
-values ('1984', 'George Orwell', 'Science Fiction', '120.99');
-
-INSERT INTO Books
-    (title, author, genre, price)
-values ('The Catcher in the Rye', 'J.D. Salinger', 'Fiction', '100.99');
-
-INSERT INTO Books
-    (title, author, genre, price)
-values ('To Kill a Mockingbird', 'Harper Lee', 'Fiction', '140.99');
-
-INSERT INTO Books
-(title, author, genre, price)
-values ('Call of Cthulu', 'Howard P. Lovecraft', 'Science Fiction', '70.99');
-
-INSERT INTO Books
-(title, author, genre, price)
-values ('The Raven', 'Edgar Allan Poe', 'Horror', '70.99');
-
-INSERT INTO Books
-(title, author, genre, price)
-values ('Call of Cthulu', 'Howard P. Lovecraft', 'Science Fiction', '70.99');
-
-SELECT *
-from Books;
-
-CREATE OR REPLACE TABLE Books
-(                                          -- auto-incrementing kronologiskai prideda nauja skaiciu, naujam item'sui pvz. knygai
-    id     INT AUTO_INCREMENT PRIMARY KEY, -- a primary key is similar kind of to IDs, but PKs do not need to be numbers, just a way to assign a unique identifier
-    title  text(512),                      -- char by default is 256
-    author text(512),
-    genre  text(128),
-    price  dec
-);
-
+VALUES ('1984', 'George Orwell', 'Science Fiction', 120.99),
+       ('The Catcher in the Rye', 'J.D. Salinger', 'Fiction', 100.99),
+       ('To Kill a Mockingbird', 'Harper Lee', 'Fiction', 140.99),
+       ('Call of Cthulhu', 'Howard P. Lovecraft', 'Science Fiction', 70.99),
+       ('The Raven', 'Edgar Allan Poe', 'Horror', 70.99),
+       ('Call of Cthulhu', 'Howard P. Lovecraft', 'Science Fiction', 70.99);
 
 INSERT INTO Customers
-    (id, name, address, telephone_number, purchased_books)
-VALUES (1, 'Lukas Petrauskas', 'Gedimino pr. 12, Vilnius, Lithuania', '+370 5 212 3456', 3),
-       (2, 'Freja Jensen', 'Nørrebrogade 45, 2200 København N, Denmark', '+45 35 30 12 34', 1),
-       (3, 'Harry Mason', 'Elm Street 13, USA', '555 019 2834', 10);
-        (4, 'Silent Bob', 'Hell Street 12, USA', '555 019 2774', 1);
+    (name, address, telephone_number, purchased_books)
+VALUES ('Lukas Petrauskas', 'Gedimino pr. 12, Vilnius, Lithuania', '37052123456', 3),
+       ('Freja Jensen', 'Nørrebrogade 45, 2200 København N, Denmark', '4535301234', 1),
+       ('Harry Mason', 'Elm Street 13, USA', '5550192834', 17),
+       ('Silent Bob', 'Hell Street 12, USA', '5550192774', 1),
+       ('James Sunderland', 'Silent Hill Street, USA', '5550192666', 10);
